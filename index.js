b23dc45508b53ccbb37c44f9b94f7b79f373d16f
Taifu <752761705@qq.com>
2018-05-09 18:32:29 +0800

    pay10

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ var url = require("url");
 
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
+app.get('/', function(req, res, next){
   res.sendFile(__dirname + '/index.html');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
 app.get('/pay10', function(request, res){
     var emitId = url.parse(request.url,true).query.emitId;
@@ -22,12 +25,14 @@ app.get('/pay10', function(request, res){
     });
     res.end();
 });
-
+io.set('transports', [ 'websocket' ]);
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
+io.origins(['http://127.0.0.1:8080','https://wuchaolong.github.io/video/']);
+
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
